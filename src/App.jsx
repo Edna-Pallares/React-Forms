6ea1cad6379cc6538499209db2b5e25f8c9b3601
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-02-05 22:05:54 -0500

    Sign Up Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-
-}
-
-export default App
+import { useState } from 'react';
+import './App.css';
+import SignUpForm from './components/SignUpForm';
+import Authenticate from './components/Authenticate';
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+  }
